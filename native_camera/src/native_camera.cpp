24673e76cae8995a44c5434e47ffe3f985ff1aa5
Oleg <egsov@mail.ru>
2025-03-10 15:50:30 +0500

    Update native_camera.cpp

--- native_camera/src/native_camera.cpp
+++ native_camera/src/native_camera.cpp
@@ -218,8 +218,8 @@ void Camera::worldToScreen(dmVMath::Vector3 worldPos, float *outX, float *outY){
     dmVMath::Vector4 pv = dmVMath::Vector4(worldPos.getX(), worldPos.getY(), worldPos.getZ(), 1);
     pv = m * pv;
     pv = pv * (1/pv.getW());
-    *outX = pv.getX() / 2 * viewAreaWidth + 0.5 * viewAreaWidth;
-    *outY = pv.getY() / 2 * viewAreaHeight + 0.5 * viewAreaHeight;
+    *outX = (pv.getX() / 2 * viewAreaWidth + 0.5 * viewAreaWidth) / (viewAreaWidth / screenWidth);
+    *outY = (pv.getY() / 2 * viewAreaHeight + 0.5 * viewAreaHeight) / (viewAreaHeight / screenHeight);
 }
 
 // Implementation of the updated toString method
